bb2523156f3a17475a1068a367141a638056b50d
Andrea Sutton <andrea.sutton@hotmail.com.au>
2024-09-17 13:09:38 +1000

    Created main py, classes, and read me with introduction

---
+++ main.py
@@ -0,0 +1,4 @@
+#first going to set up the welcome to app message and import color module which will make terminal easier to read.
+# from colored import Fore, Back, Style
+
+# print(f"{Fore.red}{Back.black}Welcome to the D&D Dice Application{Style.reset}\n")
